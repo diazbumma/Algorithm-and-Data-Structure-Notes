f5c2fe05ddb96e741a465782c97ce9bff4829c5f
Odiaz Bumma <odiazbumma@gmail.com>
2019-11-07 16:27:08 +0700

    Update merge sort

--- src/MergeSort.java
+++ src/MergeSort.java
@@ -4,49 +4,50 @@ public class MergeSort {
 
     public static void main(String[] args) {
         int[] myArray = {7, 14, 3, 5, 13, 10, 11, 6, 9, 4, 1, 8, 12, 2, 15};
-        mergeSort(myArray,  myArray.length);
+        mergeSort(myArray,  0, myArray.length-1);
         System.out.println(Arrays.toString(myArray));
     }
 
-    //n denotes array size which will be sorted
-    public static void mergeSort(int[] intArray, int n) {
-        if (n < 2)
+    public static void mergeSort(int[] arr, int left, int right) {
+        if (arr.length == 1)
             return;
-        int mid = n/2;
-        int[] leftArr = new int[mid];
-        int[] rightArr = new int[n-mid];
-        for (int i = 0; i < mid; i++)
-            leftArr[i] = intArray[i];
-        for (int i = mid; i < n; i++)
-            rightArr[i - mid] = intArray[i];
-        mergeSort(leftArr, mid);
-        mergeSort(rightArr, n-mid);
-        merge(intArray, leftArr, rightArr, mid, n-mid);
+        int mid = (right+left)/2;
+        int leftSize = mid-left+1;
+        int rightSize = right-mid;
+        int[] leftSide = new int[leftSize];
+        int[] rightSide = new int[rightSize];
+        for (int i = 0; i < leftSize; i++)
+            leftSide[i] = arr[i];
+        for (int i = 0; i < rightSize; i++)
+            rightSide[i] = arr[i+(mid+1)];
+        mergeSort(leftSide, 0, leftSize-1);
+        mergeSort(rightSide, 0, rightSize-1);
+        mergeArray(arr, leftSide, rightSide, leftSize, rightSize);
     }
 
-    public static void merge(int[] intArray, int[] leftArr, int[] rightArr, int left, int right) {
-        int i = 0;
-        int j = 0;
-        int k = 0;
-        while (i < left && j < right) {
-            if (leftArr[i] <= rightArr[j]) {
-                intArray[k] = leftArr[i];
-                i++;
+    public static void mergeArray(int[] arr, int[] leftside, int[] rightSide, int leftSize, int rightSize) {
+        int iArr = 0;
+        int iLeft = 0;
+        int iRight = 0;
+        while (iLeft<leftSize && iRight<rightSize) {
+            if (leftside[iLeft] < rightSide[iRight]) {
+                arr[iArr] = leftside[iLeft];
+                iLeft++;
             } else {
-                intArray[k] = rightArr[j];
-                j++;
+                arr[iArr] = rightSide[iRight];
+                iRight++;
             }
-            k++;
+            iArr++;
         }
-        while (i<left) {
-            intArray[k] = leftArr[i];
-            i++;
-            k++;
+        while (iLeft < leftSize) {
+            arr[iArr] = leftside[iLeft];
+            iArr++;
+            iLeft++;
         }
-        while (j<right) {
-            intArray[k] = rightArr[j];
-            j++;
-            k++;
+        while (iRight < rightSize) {
+            arr[iArr] = rightSide[iRight];
+            iArr++;
+            iRight++;
         }
     }
 }
