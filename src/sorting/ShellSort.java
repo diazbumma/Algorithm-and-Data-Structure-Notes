bb862f4cee78dbe7e4a6af9a58daddcb9f001332
Odiaz Bumma <odiazbumma@gmail.com>
2019-12-25 19:59:54 +0700

    Add shell sort example

---
+++ src/sorting/ShellSort.java
@@ -0,0 +1,34 @@
+package sorting;
+
+import java.util.Arrays;
+
+public class ShellSort {
+
+    public static void main(String[] args) {
+        int[] myArray = {7, 14, 3, 5, 13, 10, 11, 6, 9, 4, 1, 8, 12, 2, 15};
+        shellSort(myArray);
+        System.out.println(Arrays.toString(myArray));
+    }
+
+    public static void shellSort(int[] intArray) {
+        int size = intArray.length;
+        int gap = size/2;
+
+        for (int i = gap; i < size; i++) {
+            int temp = intArray[i];
+            int key = i;
+
+            while (key-gap >= 0 && temp < intArray[key-gap]) {
+                intArray[key] = intArray[key-gap];
+                key -= gap;
+            }
+
+            intArray[key] = temp;
+
+            if (i == size-1 && gap > 1) {
+                gap /= 2;
+                i = gap-1;
+            }
+        }
+    }
+}
